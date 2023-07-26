18b35ac1dc49daa56e600cf42227b92643cf9491
Pavel Admaikin <admaikin.pavel@gmail.com>
2023-07-26 16:36:11 +0500

    Added basic logic and tests

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/Dor1ma/Basic-http-rest-api/internal/app/apiserver"
+	"log"
+	"os"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.json", "path to config file")
+}
 
 func main() {
-	fmt.Println("HELLO WORLD")
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+
+	content, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(config)
+
+	s := apiserver.New(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,1 +1,59 @@
 package apiserver
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
+
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIServer) Start() error {
+	if err := s.configLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	s.logger.Info("starting api server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	// место, где можно определять переменные, которые будут использованы только в данном хэндлере
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// здесь описывыется логика обработки каждого конкретного запроса
+
+		io.WriteString(w, "Hello")
+	}
+}
